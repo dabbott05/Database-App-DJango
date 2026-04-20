f9a796b59401f8f799363d460295e1160ddea2e9
Daniel Abbott <145143890+dabbott05@users.noreply.github.com>
2026-04-20 10:27:06 +0000

    Mission 4, 5, and some of 6

--- mythical_mane/admin.py
+++ mythical_mane/admin.py
@@ -1,3 +1,40 @@
 from django.contrib import admin
+from .models import Universe, Owner, Patient, Employee, Visit, Diagnosis, ProcedureDefinition
 
-# Register your models here.
+@admin.register(Universe)
+class UniverseAdmin(admin.ModelAdmin):
+    list_display = ('universe_id', 'name')
+    search_fields = ('name',)
+
+@admin.register(Owner)
+class OwnerAdmin(admin.ModelAdmin):
+    list_display = ('owner_id', 'name', 'phone', 'email')
+    search_fields = ('name', 'email', 'phone')
+
+@admin.register(Patient)
+class PatientAdmin(admin.ModelAdmin):
+    list_display = ('patient_id', 'name', 'color', 'dob', 'owner', 'universe')
+    search_fields = ('name', 'color')
+    list_filter = ('universe',)
+
+@admin.register(Employee)
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ('employee_id', 'name', 'job_role', 'specialty')
+    search_fields = ('name', 'job_role')
+    list_filter = ('job_role',)
+
+@admin.register(Visit)
+class VisitAdmin(admin.ModelAdmin):
+    list_display = ('visit_id', 'patient', 'vet', 'start_at', 'reason')
+    list_filter = ('start_at', 'vet')
+    date_hierarchy = 'start_at'
+
+@admin.register(Diagnosis)
+class DiagnosisAdmin(admin.ModelAdmin):
+    list_display = ('diagnosis_id', 'name', 'code')
+    search_fields = ('name', 'code')
+
+@admin.register(ProcedureDefinition)
+class ProcedureDefinitionAdmin(admin.ModelAdmin):
+    list_display = ('procedure_id', 'name', 'standard_cost')
+    search_fields = ('name',)
--- mythical_mane/views.py
+++ mythical_mane/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Patient
 
-# Create your views here.
+def patient_list(request):
+    # 'select_related' is the magic word here. It tells Django to grab the 
+    # Owner and Universe data at the EXACT SAME TIME as the Patient data. 
+    # This solves the "avoid one query per patient" grading requirement!
+    patients = Patient.objects.select_related('owner', 'universe').all()
+    
+    return render(request, 'patient_list.html', {'patients': patients})
